51dcba9d2bed5ef3ca77114485791ba6c12bbdc3
Jayateerth Dambal <jayateerth.dambal@gmail.com>
2023-01-30 10:27:01 +0530

    Adding Stage2 and 3 Test Pages and Logic

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('logout/', views.logout_page, name='logout'),
     path('take_test/', views.record_response, name='record_res'),
     path("analyze_response/<int:id>", views.analyze_user_response, name="analyze_response"),
-
+    path("stage_2_test/", views.stage2_test_response, name="stage2Test"),
+    path("stage_3_test/", views.stage3_test_response, name="stage3Test"),
     # path('')
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -74,13 +74,68 @@ def record_response(request):
 
 
 def analyze_user_response(response, id):
+    global df
     test_data = UserResponse.objects.values().get(pk=id)
     test_data_json = dumps(test_data, default=str)
     vals = [test_data[i] for i in test_data]
+    stage_num = vals[2]
     vals = vals[3:8]
-    df = pd.read_csv('Datasets/Stage-1.csv')
+    if stage_num == 1:
+        print("Stage-1")
+        df = pd.read_csv('Datasets/Stage-1.csv')
+    elif stage_num == 2:
+        print("Stage-2")
+        df = pd.read_csv('Datasets/Stage-2.csv')
+    elif stage_num == 3:
+        print("Stage-3")
+        df = pd.read_csv('Datasets/Stage-3.csv')
+
     proc_df = df.loc[:, df.columns != 'Answers']
     proc_df.loc[len(proc_df)] = vals
     dup_df = proc_df[proc_df.duplicated(keep=False)]
     result = df.iloc[dup_df.index[0]]['Answers']
     return render(response, 'analyze_test.html', {'result': result})
+
+
+@login_required(login_url='/login')
+def stage2_test_response(request):
+    form = UserResponseForm()
+
+    if request.method == 'POST':
+        form = UserResponseForm(request.POST)
+        first_ques = request.POST.get("first_ques")
+        second_ques = request.POST.get("second_ques")
+        third_ques = request.POST.get("third_ques")
+        fourth_ques = request.POST.get("fourth_ques")
+        fifth_ques = request.POST.get("fifth_ques")
+        request.user.userresponse_set.create(stage_number=2, first_ques=first_ques, second_ques=second_ques,
+                                             third_ques=third_ques, fourth_ques=fourth_ques, fifth_ques=fifth_ques)
+
+        user_response = UserResponse.objects.filter(user=request.user).latest("date_time")
+
+        return render(request, 'test_view.html', {'data': user_response, 'form': form})
+
+    else:
+        return render(request, 'Stage-2_test.html', {"form": form})
+
+
+@login_required(login_url='/login')
+def stage3_test_response(request):
+    form = UserResponseForm()
+
+    if request.method == 'POST':
+        form = UserResponseForm(request.POST)
+        first_ques = request.POST.get("first_ques")
+        second_ques = request.POST.get("second_ques")
+        third_ques = request.POST.get("third_ques")
+        fourth_ques = request.POST.get("fourth_ques")
+        fifth_ques = request.POST.get("fifth_ques")
+        request.user.userresponse_set.create(stage_number=3, first_ques=first_ques, second_ques=second_ques,
+                                             third_ques=third_ques, fourth_ques=fourth_ques, fifth_ques=fifth_ques)
+
+        user_response = UserResponse.objects.filter(user=request.user).latest("date_time")
+        return render(request, 'test_view.html', {'data': user_response, 'form': form})
+
+
+    else:
+        return render(request, 'Stage-3_test.html', {"form": form})
